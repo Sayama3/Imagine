8ebb6b202980c3a4089393927278059e59a13c54
Iannis Poquet <ianpo@live.fr>
2025-05-10 17:19:30 +0200

    RTTI Finish Making
    TODO: create a example usecase working

--- Core/Includes/Imagine/Core/RTTI.hpp
+++ Core/Includes/Imagine/Core/RTTI.hpp
@@ -3,125 +3,124 @@
 //
 
 #pragma once
+#include "Macros.hpp"
+#include "RTTI.hpp"
 
-// namespace Imagine::Core {
-//
-// 	template<class Base>
-// 	struct RTTI {
-// 		RTTI() = default;
-// 		~RTTI() = default;
-// 		explicit RTTI(std::string name, const RTTI<Base>* parent_rtti) : type(std::move(name)), parent(parent_rtti) {}
-// 		explicit RTTI(const char* cname, const RTTI<Base>* parent_rtti) : type(cname), parent(parent_rtti) {}
-//
-// 		bool IsExactlyA(const RTTI<Base>& other) const;
-// 		bool IsA(const RTTI<Base>& other) const;
-//
-// 		const std::string type;
-// 		const RTTI<Base>* const parent;
-// 	};
-//
-// 	template<class Base>
-// 	bool RTTI<Base>::IsExactlyA(const RTTI<Base>&other) const {
-// 		return type == other.type && parent == other.parent;
-// 	}
-//
-// 	template<class Base>
-// 	bool RTTI<Base>::IsA(const RTTI<Base> &other) const {
-// 		const RTTI<Base>* comp = this;
-// 		do {
-// 			if (other.IsExactlyA(comp)) return true;
-// 			comp = comp->parent;
-// 		} while (parent != nullptr);
-// 		return false;
-// 	}
-// }
-//
-// #define RTTI_DECLARATION(base) static ::Imagine::Core::RTTI<base> rtti;
-// #define RTTI_DEFINITION_BASE(base) ::Imagine::Core::RTTI<base> base::rtti{#base , nullptr};
-// #define RTTI_DEFINITION(type, base, parent) ::Imagine::Core::RTTI<base> type::rtti{#type, &parent::rtti};
+namespace Imagine::Core {
+
+ 	template<class Base>
+ 	struct RTTI {
+ 	private:
+ 		static inline std::unordered_map<std::string, Base*(*)()> Factory{};
+ 	public:
+ 		RTTI() = default;
+ 		~RTTI() = default;
+
+ 		template<typename Self>
+ 		inline RTTI() : type(typeid(Self).name()), parent(nullptr), createFunc(Self::Create) {
+ 			MGN_TRACE(type);
+ 			Initialise();
+ 		}
+
+ 		template<typename Self, typename ParentType>
+ 		inline RTTI() : type(typeid(Self).name()), parent(ParentType::rtti), createFunc(Self::Create) {
+ 			MGN_TRACE(type);
+ 			Initialise();
+ 		}
+
+ 		inline explicit RTTI(std::string name, Base*(*createFunc)(), const RTTI<Base>* parent_rtti) : type(std::move(name)), parent(parent_rtti), createFunc(createFunc) {
+ 			MGN_TRACE(type);
+ 			Initialise();
+ 		}
+
+ 		bool IsRoot() const {return !parent || parent==this;}
+ 		bool IsExactlyA(const RTTI<Base>& other) const;
+ 		bool IsA(const RTTI<Base>& other) const;
+ 	public:
+ 		Base* Create() {return createFunc();}
+ 		static inline Base* FactoryCreate(const std::string& type) {return Factory.contains(type) ? Factory.at(type)() : nullptr;}
+ 		static inline Base* FactoryCreate(const RTTI<Base>& rtti) {return Factory.contains(rtti.type) ? Factory.at(rtti.type)() : nullptr;}
+ 	private:
+ 		void Initialise()
+ 		{
+ 			Factory.insert({type, createFunc});
+ 		}
+ 	public:
+ 		const std::string type;
+ 		const RTTI<Base>* const parent;
+ 	private:
+ 		Base* (*createFunc) () const;
+ 	};
+
+ 	template<class Base>
+ 	bool RTTI<Base>::IsExactlyA(const RTTI<Base>&other) const {
+ 		return type == other.type && parent == other.parent;
+ 	}
+
+ 	template<class Base>
+ 	bool RTTI<Base>::IsA(const RTTI<Base> &other) const {
+ 		const RTTI<Base>* comp = this;
+ 		do {
+ 			if (other.IsExactlyA(comp)) return true;
+ 			comp = comp->parent;
+ 		} while (parent != nullptr);
+ 		return false;
+ 	}
+}
+
+template<class Base>
+struct ::std::hash<::Imagine::Core::RTTI<Base>>
+{
+	std::size_t operator()(const ::Imagine::Core::RTTI<Base>& s) const noexcept
+	{
+		const std::size_t h1 = std::hash<std::string>{}(s.type);
+		const std::size_t h2 = std::hash<uint64_t>{}(s.parent);
+		return h1 ^ (h2 << 1);
+	}
+};
+
+#define RTTI_IMPLEMENT_CREATE(Base, Self) inline static Base* Create() { return new Self(); }
+#define RTTI_ADVANCED_DECLARATION(Base, Self, TypeNameStr, Parent) inline static ::Imagine::Core::RTTI<Base> rtti{TypeNameStr, Self::Create, (typeid(Self)==typeid(Parent)?nullptr:&Parent::rtti)}
+
+#define RTTI_DECLARATION(Base, Self, Parent) RTTI_IMPLEMENT_CREATE(Base, Self) RTTI_ADVANCED_DECLARATION(Base, Self, #Self, Parent)
+#define RTTI_ROOT_DECLARATION(Base) RTTI_DECLARATION(Base, Base, Base)
 
 
 namespace Imagine::Core {
 
-	template<class Base>
-	class RTTIBase {
-	public:
-		virtual ~RTTIBase() = default;
-		RTTIBase() : parent(nullptr) {}
-		explicit RTTIBase(const RTTIBase* parent) : parent(parent) {}
+	class Something;
+	using RTTISomething = ::Imagine::Core::RTTI<Something>;
 
-		[[nodiscard]] virtual bool IsRoot() const {
-			return parent == nullptr;
-		}
-		[[nodiscard]] virtual bool IsExactlyA(const RTTIBase& other) const {
-			return this == &other;
-		}
-		[[nodiscard]] virtual bool IsA(const RTTIBase& other) const {
-			const RTTIBase* comp = this;
-			do {
-				if (other.IsExactlyA(*comp)) return true;
-				comp = comp->parent;
-			} while (comp != nullptr);
-			return false;
-		}
+ 	#define RTTISomething_DECLARATION(Self, Parent) RTTI_DECLARATION(::Imagine::Core::Something, Self, Parent)
 
-		const RTTIBase* const parent;
-	};
-
-	template<class Self, class Parent, class Base>
-	class RTTI {
-	public:
-		// inline static RTTIBase<Base> rtti {typeid(Self)==typeid(Parent) ? nullptr : &typename Parent::rtti};
-	public:
-		[[nodiscard]] inline static bool StaticIsRoot() {
-			return typeid(Self) == typeid(Parent);
-		}
-		[[nodiscard]] inline bool IsRoot() const {
-			return StaticIsRoot();
-		}
-
-		template<class OtherSelf, class OtherParent>
-		[[nodiscard]] inline static bool StaticIsExactlyA(const RTTI<OtherSelf, OtherParent, Base>& other) {
-			return typeid(Self) == typeid(OtherSelf) && typeid(Parent) == typeid(OtherParent);
-		}
-		template<class OtherSelf, class OtherParent>
-		[[nodiscard]] inline bool IsExactlyA(const RTTI<OtherSelf, OtherParent, Base>& other) const {
-			return StaticIsExactlyA(other);
-		}
-
-		template<class OtherSelf, class OtherParent>
-		[[nodiscard]] inline static bool StaticIsA(const RTTI<OtherSelf, OtherParent, Base>& other) {
-			return StaticIsExactlyA(other) || (!StaticIsRoot() && Parent::StaticIsA(other));
-		}
-		template<class OtherSelf, class OtherParent>
-		[[nodiscard]] inline bool IsA(const RTTI<OtherSelf, OtherParent, Base>& other) const {
-			return StaticIsA(other);
-		}
-	};
-
-
-	class Something : public RTTI<Something, Something, Something> {
-
+ 	class Something {
+ 	public:
+ 		RTTI_ROOT_DECLARATION(Something);
 		Something() {}
 		virtual ~Something() {};
 
 	};
 
-	class SomeOtherThing : public virtual RTTI<SomeOtherThing, Something, Something>, public virtual Something {
-		SomeOtherThing(): Something() {}
-
+	class SomeOtherThing : public Something {
+	public:
+		RTTISomething_DECLARATION(SomeOtherThing, Something);
+		SomeOtherThing() {}
 		virtual ~SomeOtherThing() override {};
 	};
 
-	class AnotherThing : public RTTI<AnotherThing, Something, Something>, public virtual Something {
-		AnotherThing(): Something() {}
-
+	class AnotherThing : public Something {
+	public:
+		RTTISomething_DECLARATION(AnotherThing, Something);
+		AnotherThing() {}
 		virtual ~AnotherThing() override {};
 	};
 
-	class AnotherSomeOtherThing : public RTTI<AnotherSomeOtherThing, SomeOtherThing, Something>, public virtual SomeOtherThing {
-		AnotherSomeOtherThing(): SomeOtherThing() {}
-
+	class AnotherSomeOtherThing : public SomeOtherThing {
+	public:
+		RTTISomething_DECLARATION(AnotherSomeOtherThing, SomeOtherThing);
+		AnotherSomeOtherThing()
+		{
+		}
 		virtual ~AnotherSomeOtherThing() override {};
 	};
 }
