83bdfb4604a872ce49fea334998d4f457218550b
Iannis Poquet <ianpo@live.fr>
2025-06-01 15:30:28 +0200

    Add SPIR-V shaders bytecode

--- Assets/compile.bat
+++ Assets/compile.bat
@@ -1,3 +1,4 @@
 glslc.exe .\shader.vert -o shader.vert.spv
 glslc.exe .\shader.frag -o shader.frag.spv
 glslc.exe .\gradient.comp -o gradient.comp.spv
+glslc.exe .\sky.comp -o sky.comp.spv
--- Assets/compile.sh
+++ Assets/compile.sh
@@ -1,3 +1,4 @@
 glslc shader.vert -o shader.vert.spv
 glslc shader.frag -o shader.frag.spv
 glslc gradient.comp -o gradient.comp.spv
+glslc sky.comp -o sky.comp.spv
