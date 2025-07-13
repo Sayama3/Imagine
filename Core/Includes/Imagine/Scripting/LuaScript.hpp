092a0a15b7508146dede43c5f13098c2da6a0503
Iannis Poquet <ianpo@live.fr>
2025-07-13 20:43:00 +0200

    Prepare Lua Scripting

---
+++ Core/Includes/Imagine/Scripting/LuaScript.hpp
@@ -0,0 +1,13 @@
+//
+// Created by ianpo on 13/07/2025.
+//
+
+#pragma once
+
+namespace Imagine {
+
+class LuaScript {
+
+};
+
+} // Imagine
---
+++ Core/Sources/Scripting/LuaScript.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ianpo on 13/07/2025.
+//
+
+#include "../../Includes/Imagine/Scripting/LuaScript.hpp"
+
+namespace Imagine {
+} // Imagine
